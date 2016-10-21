1f74bb9a10518dc48a890cd5aec0cb5d06324c82
Asko Soukka <asko.soukka@iki.fi>
2016-10-21 09:13:55 +0300

    Skip property sheet lookup for groups

--- src/pas/plugins/ldap/plugin.py
+++ src/pas/plugins/ldap/plugin.py
@@ -548,7 +548,9 @@ class LDAPPlugin(BasePlugin):
         """
         ugid = user_or_group.getId()
         try:
-            if ugid in self.getGroupIds() or self.users[ugid]:
+            if ugid in self.getGroupIds():
+                return {'title': ugid}
+            elif ugid in self.getGroupIds() or self.users[ugid]:
                 return LDAPUserPropertySheet(user_or_group, self)
         except KeyError:
             pass
